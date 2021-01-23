d1d38518be743b464e2eebb0b81aa59a7b4f8901
PraveenSaravanan <61940883+PraveenSaravanan7@users.noreply.github.com>
2021-01-23 05:17:24 +0000

    ready to host

--- src/api.js
+++ src/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 const ls=require("local-storage")
 var accessToken=ls("accessToken")
 export default axios.create({
-  baseURL: `http://localhost:3000/`,
+  baseURL: `https://fierce-inlet-80648.herokuapp.com/`,
+  // baseURL: `http://localhost:3000/`,
   timeout:3000,
   headers:{ 'Authorization': 'Bearer '+accessToken}
 });
