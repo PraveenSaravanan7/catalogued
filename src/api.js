a21e1fe78a6d60802dddc5d09418bcfaa624593b
Praveen Saravanan <praveensaravananoff@gmail.com>
2022-09-10 17:33:07 +0530

    removed all

--- src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-const ls=require("local-storage")
-var accessToken=ls("accessToken")
-export default axios.create({
-  baseURL: `https://fierce-inlet-80648.herokuapp.com/`,
-  // baseURL: `http://localhost:3000/`,
-  timeout:3000,
-  headers:{ 'Authorization': 'Bearer '+accessToken}
-});
